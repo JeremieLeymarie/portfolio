8e36db9257d9ee380939644831eea28f32074305
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2021-11-25 17:30:06 +0100

    [FIX] background color of skill bars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import './responsive.css';
-import React, { } from 'react';
+import React, { useEffect} from 'react';
 import { Route, Routes, BrowserRouter as Router, HashRouter } from "react-router-dom";
 import LanguageProvider from "./Components/LanguageProvider";
 import Navbar from './Components/Navbar';
@@ -9,7 +9,9 @@ import Projects from "./Components/Projects";
 import Contact from "./Components/Contact";
 
 function App() {
-
+  useEffect(()=>{
+    document.title = "Jérémie Leymarie - Full Stack Developer";
+  }, []);
   function changeActiveItem(id) {
     if (document.getElementsByClassName("active-nav-item").length > 0){
       document.getElementsByClassName("active-nav-item")[0].classList.remove("active-nav-item");
