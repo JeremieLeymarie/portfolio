ce9058de8d24123a3d4b5ca92e43f32599cbac62
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2021-11-25 17:15:37 +0100

    deployment test

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <div className="App">
-      <HashRouter base="/">
+      <HashRouter basename="/">
         <LanguageProvider>
           <Navbar click={changeActiveItem} />
           <Routes>
